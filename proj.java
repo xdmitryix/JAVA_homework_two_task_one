e767e2fddda55429989040fff327007a64ad637f
dmitry <rbdeman@yandex.ru>
2023-03-06 23:24:12 +0300

    добавил сортировку

--- proj.java
+++ proj.java
@@ -8,5 +8,21 @@ public class proj {
     public static void main(String[] args) {
         int[] numbers =  new int[] {4,2,1,6,5,8,9,7,3};
         System.out.println(Arrays.toString(numbers));
+        int [] arrResult = quickSort(numbers);
+        System.out.println(Arrays.toString(arrResult));
+    }
+
+    public static int[] quickSort(int[] array){
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] < array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
