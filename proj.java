f4cfd37914659e0e002d3c6653abc35405ce7f85
dmitry <rbdeman@yandex.ru>
2023-03-06 23:14:25 +0300

    создал файл певрой задачи

---
+++ proj.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+// Реализуйте алгоритм сортировки пузырьком числового массива (введён вами),
+// результат после каждой итерации запишите в лог-файл.
+
+public class proj {
+
+    public static void main(String[] args) {
+        int[] numbers =  new int[] {4,2,1,6,5,8,9,7,3};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
